c7943f97d1061f382b6414dab7f0e31e97b13238
tmochamer <tmoch28@gmail.com>
2017-02-21 09:38:35 -0600

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Account
+    {
+        private string name;
+        // method that sets the account name in the object
+        public void SetName(string accountName)
+        {
+            name = accountName;
+        }
+        public string GetName()
+        {
+            return name; 
+        }
+    } 
+}
+
---
+++ BankAccount/Accounttest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class AccountTest
+    {
+        static void Main()
+        {
+            // create an Account object and assign it to myAccount
+            Account myAccount = new Account();
+
+            // display myAccount's inital name (there is not one yet) 
+            Console.WriteLine($"Inital name is: {myAccount.GetName()}");
+
+            // prompt for and read the name, then put the name in the Account object
+            Console.Write("Enter the account name: ");
+            string theName = Console.ReadLine();
+            myAccount.SetName(theName);
+
+            // display the name stored inthe myAccount object
+            Console.WriteLine($"MyAccount's name is: {myAccount.GetName()}");
+        }
+    }
+}
