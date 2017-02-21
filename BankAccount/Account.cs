7434afceba146ef13e7f4fd9b1389bd3b1d1e438
tmochamer <tmoch28@gmail.com>
2017-02-21 15:54:32 -0600

    end of chapter 4

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -8,18 +8,44 @@ namespace BankAccount
 {
     class Account
     {
-        private string name;
-        // method that sets the account name in the object
-        public string Name
+        public string Name{ get; set;}
+        private decimal balance;
+        public Account  (string accountName, decimal initalBalance)
+        {
+            Name = accountName;
+            balance = initalBalance;
+        }
+        public decimal Balance
         {
             get
             {
-                return name;
+                return balance;
             }
-            set
+            private set
             {
-                name = value;
+                if (value > 0.0m)
+                {
+                    balance = value;
+                }
             }
         }
-    }
-}
+        public void Deposit(decimal depositAmount)
+        {
+            if (depositAmount > 0.0m)
+            {
+                Balance = Balance + depositAmount;
+            }
+        }
+        public void Withdraw(decimal withdrawAmount)
+            {
+            if (withdrawAmount > Balance)
+            {
+                Console.WriteLine("you have insufficant funds"); //do i want to do this in the Account Class?  or Return to Main to Console.WriteLine? 
+            }
+            else if (withdrawAmount <= Balance)
+            {
+                Balance = Balance - withdrawAmount; 
+            }
+        }
+     }  
+ }
--- BankAccount/Accounttest.cs
+++ BankAccount/Accounttest.cs
@@ -11,18 +11,47 @@ namespace BankAccount
         static void Main()
         {
             // create an Account object and assign it to myAccount
-            Account myAccount = new Account();
+            //Account myAccount = new Account();
+            Account account1 = new Account("todd mochamer", 200.00m);
+            Account account2 = new Account("steph mochamer",2000.00m);
+
 
             // display myAccount's inital name (there is not one yet) 
-            Console.WriteLine($"Inital name is: {myAccount.Name}");
+            //Console.WriteLine($"Inital name is: {myAccount.Name}");
 
             // prompt for and read the name, then put the name in the Account object
-            Console.Write("Enter the account name: ");
-            string theName = Console.ReadLine();
-            myAccount.Name = theName;
+            //Console.Write("Enter the account name: ");
+            //string theName = Console.ReadLine();
+            ///myAccount.Name = theName;
 
             // display the name stored inthe myAccount object
-            Console.WriteLine($"MyAccount's name is: {myAccount.Name}");
+            Console.WriteLine($"account1 name is: {account1.Name}");
+            Console.WriteLine($"account2 name is: {account2.Name}");
+
+            //display intal balance of each Account object
+            Console.WriteLine($"the balance for {account1.Name} is {account1.Balance:C}");
+            Console.WriteLine($"the balance for {account2.Name} is {account2.Balance:C}");
+
+            // prompt for a deposit valuse input
+            Console.Write($"Ener deposit ammount for {account1.Name}: ");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:C} to {account1.Name}");
+
+            account1.Deposit(depositAmount); //adds to balance using public method
+
+            Console.WriteLine($"{account1.Name}'s Balance = {account1.Balance:C}");
+
+            //prompt for a withdraw ammount
+            Console.Write($"how much do you want to withdraw?");
+            decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+            
+            //subtrat from acount using method Withdraw
+            account2.Withdraw(withdrawAmount);
+
+            Console.WriteLine($"{account2.Name}'s Balance = {account2.Balance:C}");
+            // lastline.
+            Console.ReadLine();
+
         }
     }
 }
